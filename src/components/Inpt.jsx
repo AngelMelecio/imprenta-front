5bb73b1c4b19548908466f1bbe06463be296e565
AngelMelecio <joseangelsm50@gmail.com>
2024-01-23 10:59:10 -0600

    crud precios

--- src/components/Inpt.jsx
+++ src/components/Inpt.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { MyIcons } from '../constants/Icons'
 
-const Inpt = ({ label, name, formik, ...props }) => {
+const Inpt = ({ label, name, formik, value = null, showErrors = true, format, ...props }) => {
 
     const [isFocus, setIsFocus] = useState(false)
     const [hasValue, setHasValue] = useState(false)
@@ -11,19 +11,21 @@ const Inpt = ({ label, name, formik, ...props }) => {
 
         setError(formik?.errors[name] && formik?.touched[name])
         setHasValue(formik?.values[name])
-    }, [formik])
+
+    }, [formik.values, formik.errors, formik.touched])
 
     const handleFocus = (e) => {
         if (props.readOnly) return
         setIsFocus(true)
     }
     const handleBlur = (e) => {
-        setIsFocus(false)
+        setIsFocus(false);
         if (e.target.value) {
             setHasValue(true);
         } else {
             setHasValue(false);
         }
+        formik?.handleBlur(e)
     }
 
     return (
@@ -32,19 +34,25 @@ const Inpt = ({ label, name, formik, ...props }) => {
                 <label htmlFor={name}
                     className={`absolute  bg-white px-1 pointer-events-none 
                             ${error ? 'text-rose-400' : isFocus && !props.readOnly ? 'text-emerald-600' : 'text-gray-500'} ${isFocus || hasValue ? 'up' : ''} transition-all duration-200 `}>{label}</label>
+                {format === 'currency' && <span className="absolute text-gray-500 transform -translate-y-1/2 left-4 top-1/2">$</span>}
                 <input
                     id={name}
+                    name={name}
                     onFocus={handleFocus}
                     onChange={formik?.handleChange}
-                    value={formik?.values[name] || ""}
-                    onBlur={(e) => { handleBlur(e); formik?.handleBlur(e) }}
+                    value={value || formik?.values[name] || ""}
+                    onBlur={handleBlur}
                     className={`w-full px-4 py-2 text-base text-gray-700 border rounded-lg outline-none  duration-200 font-medium 
-                    ${error ? 'border-rose-400' : isFocus ? 'border-emerald-500' : !props.readOnly ? 'hover:border-emerald-500' : ''} `}
+                    ${error ? 'border-rose-400' : isFocus ? 'border-emerald-500' : !props.readOnly ? 'hover:border-emerald-500' : ''} 
+                    ${format === 'currency' ? 'pl-8' : ''}
+                    `}
                     {...props} />
-            </div>
-            <div className={`flex pl-1 text-sm h-9 text-rose-400 ${error ? 'opacity-100' : 'opacity-0'} duration-200`}>
-                {error && <><MyIcons.Info style={{ margin: '3px' }} />{formik.errors[name]}</>}
-            </div>
+            </div>{
+                showErrors &&
+                <div className={`flex pl-1 text-sm h-9 text-rose-400 ${error ? 'opacity-100' : 'opacity-0'} duration-200`}>
+                    {error && <><MyIcons.Info style={{ margin: '3px' }} />{formik.errors[name]}</>}
+                </div>
+            }
         </div>
     )
 }
